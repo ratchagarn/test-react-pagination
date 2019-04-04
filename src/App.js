4ee3a6d7a269a3594d8751af8f32fb3370e6c269
ratchagarn <ratchagarn@gmail.com>
2019-04-04 09:41:16 +0700

    Fix wrong merge

--- src/App.js
+++ src/App.js
@@ -6,16 +6,12 @@ import Pagination from './components/Pagination/Pagination'
 import useDataFromApi from './components/Pagination/useDataFromApi'
 
 function App() {
-<<<<<<< HEAD
-  const [currentPage, setCurrentPage] = useState(1)
-=======
   const urlParams = new URLSearchParams(window.location.search)
 
   const [currentPage, setCurrentPage] = useState(
     Number(urlParams.get('page')) || 1
   )
 
->>>>>>> Init app
   const perPage = 5
   const totalPages = 50
 
@@ -71,10 +67,7 @@ function App() {
 
   function handleOnPageChange(page) {
     setCurrentPage(page)
-<<<<<<< HEAD
-=======
     window.history.pushState(null, null, `?page=${page}`)
->>>>>>> Init app
   }
 }
 
